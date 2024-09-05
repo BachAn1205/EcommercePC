47a276f79e1c94d8555949669fb3108eb69761f5
kishor <kishorgujar95@gmail.com>
2024-09-06 01:48:14 +0530

    Dynamic products from database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
+using Ecommerce.Data;
 using Ecommerce.Models;
+using Ecommerce.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Ecommerce.Controllers
@@ -7,15 +10,21 @@ namespace Ecommerce.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var products = await _context.Products.ToListAsync();
+
+            var model = new HomeViewModel { Products = products };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
